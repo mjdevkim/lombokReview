a1a24fef8aa2342f26467ed37f102ce6938ef2a5
Minjae Kim <137mjkim@gmail.com>
2026-07-09 10:21:02 +0900

    Initial commit

---
+++ src/main/java/com/example/lombokReview/LombokReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.lombokReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokReviewApplication.class, args);
+	}
+
+}
